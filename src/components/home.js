8af4d2494250021fd1fc9aa9aca8419a9dd17f6e
r0ckst34dy <r0ck1ngst34dy@outlook.com>
2020-05-04 10:20:55 -0400

    setup app routes

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,7 @@ import Logo from "./logo"
 import SearchBar from "./search-bar";
 import RecentPost from "./recent-post";
 
-export default class App extends Component {
+export default class Home extends Component {
   render() {
     return (
       
