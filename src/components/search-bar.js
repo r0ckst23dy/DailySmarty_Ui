7718c8126e44f70fd850a818ce5e557ef1c0fcbd
r0ckst34dy <r0ck1ngst34dy@outlook.com>
2020-05-04 09:56:52 -0400

    setup redux form

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from 'redux-form';
 
+class SearchBar extends Component {
 
-class SearchBar extends Component { 
+    handleFormSubmission =function({query}) {
+        console.log("trying handle form submit query ", query);
+        
+    }
+
+    renderInput(field) {
+        return <input type="text" placeholder="Search DailySmarty" {...field.input}/>
+    }
+
     render() {
+
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
-                <input placeholder="Search DailySmarty" />
+            <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmission.bind(this))}>
+                <Field name="query" component={this.renderInput} />
             </form>
+
         )
     }
 }
+SearchBar = reduxForm({
+    form: 'SearchBar'
+})(SearchBar);
 
 export default SearchBar;
