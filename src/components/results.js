ff632e9e18a0bd6f54422f1cf7ddf8cf02a943ac
r0ckst34dy <r0ck1ngst34dy@outlook.com>
2020-05-04 10:27:00 -0400

    added results component

---
+++ src/components/results.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Logo from "./log";
+
+class Results extends Component { 
+    render() { 
+        return (
+            <div>
+                <h1>Results</h1>
+                <Logo />
+            </div>
+        ) 
+    }
+}
+
+export default Results;
