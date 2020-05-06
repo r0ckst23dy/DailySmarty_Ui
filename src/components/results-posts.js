7ce0b54bd95d9811a6f74fe4309013dc846c38ac
r0ckst34dy <r0ck1ngst34dy@outlook.com>
2020-05-06 18:28:42 -0400

    rendered results posts component and add link component to logo

--- src/components/results-posts.js
+++ src/components/results-posts.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react"; 
 import {connect} from "react-redux";
 
+import Post from "./post"
+
 class ResultsPosts extends Component { 
+    renderPosts() { 
+        const posts = this.props.posts.map((post, index) => {
+            return <Post key={index} {...post}/>
+        })
+        return posts
+    }
     render() {
         return (
             <div className="results-posts">
                 <div className="results-posts-wrapper">
                     <ul className="results-posts-posts">
-                        Results Go Here
+                        {this.renderPosts()}
                     </ul>
 
                 </div>
@@ -16,6 +24,6 @@ class ResultsPosts extends Component {
     }
 }
 function mapStateToProps(state) {
-    return {state}
+    return { posts: state.post.resultsPosts}
 }
 export default connect(mapStateToProps)(ResultsPosts);
