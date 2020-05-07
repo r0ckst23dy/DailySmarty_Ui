22b0ea76681ed00ad4844e321480d3e3d464ed86
r0ckst34dy <r0ck1ngst34dy@outlook.com>
2020-05-07 17:53:21 -0400

    added react-animate-height to add height animation to result post links

--- src/components/post.js
+++ src/components/post.js
@@ -1,7 +1,15 @@
 import React, {Component} from 'react';
+import AnimateHeight from 'react-animate-height';
+
 
 
 class Post extends Component { 
+    constructor() { 
+        super();
+
+        this.state = {height: 0}
+    }
+
     rendorTopics() { 
         let topics = this.props.associated_topics.map((topic, index) => {
             return <span className="post-topic" key={index}>{topic}</span>
@@ -46,11 +54,16 @@ class Post extends Component {
 
                     </div>
                     <div className="result-post-title">
-                        {this.props.title}
+                        <a href={this.props.url_for_post}
+                        onMouseEnter={() => this.setState({ height: 70})}
+                        onMouseLeave={() => this.setState({ height: 0})}
+                        >{this.props.title}</a>
                     </div>
-                    <div className="result-post-links">
-                        {this.renderLinks()}
-                    </div>
+                    <AnimateHeight duration={500} height={this.state.height}>
+                        <div className="result-post-links">
+                            {this.renderLinks()}
+                        </div>
+                    </AnimateHeight>
                 </li>
             )
         } 
